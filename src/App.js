045bce9d84c12b4491f0c22d0e188fb557475b38
satatianass <satatianass00@gmail.com>
2022-03-02 21:50:01 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import {Switch, Route} from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Products" component={Products} />
+          <Route exact path="/products/:id" component={Product}/>
+        </Switch>
+    </>
   );
 }
 
